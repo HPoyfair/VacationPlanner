a38b1c0b158fa9f3eaa07858fca507a47a452fc9
JCollver23 <jessicaherborn@gmail.com>
2025-01-31 17:26:58 -0800

    Added NavBar, css styling for NavBar and SavedDestinations page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,4 @@
+
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './reset.css';
@@ -9,6 +10,7 @@ import ErrorPage from './pages/ErrorPage.tsx';
 import Home from './pages/Home.tsx';
 import Login from './pages/Login.tsx';
 import Signup from './pages/Signup.tsx';
+import SavedDestinations from './pages/SavedDestinations.tsx';
 
 const router = createBrowserRouter([
   {
@@ -27,6 +29,10 @@ const router = createBrowserRouter([
       {
         path: '/signup',
         element: <Signup />
+      },
+      {
+        path: '/saved',
+        element: <SavedDestinations />
       }
     ]
   }
@@ -36,3 +42,4 @@ const rootElement = document.getElementById('root');
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
 }
+
---
+++ client/src/pages/SavedDestinations.tsx
@@ -0,0 +1,9 @@
+function SavedDestinations() {
+    return (
+        <div>
+        <h1>Saved Destinations Page</h1>
+        </div>
+    );
+}
+
+export default SavedDestinations;
