fd02a31b45120bd38f4f43f26467c479c97191f3
JCollver23 <jessicaherborn@gmail.com>
2025-02-04 23:09:53 -0800

    NavBar css and clean up

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,17 +1,24 @@
 import { useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import auth from '../utils/auth';
 import './style.css';
 
 const Navbar = () => {
+  // State to track the login status
   const [loginCheck, setLoginCheck] = useState(false);
-  const location = useLocation(); // Get current path
 
-  useEffect(() => {
+  // Function to check if the user is logged in using auth.loggedIn() method
+  const checkLogin = () => {
     if (auth.loggedIn()) {
-      setLoginCheck(true);
+      setLoginCheck(true);  // Set loginCheck to true if user is logged in
     }
-  }, [loginCheck]);
+  };
+
+  // useEffect hook to run checkLogin() on component mount and when loginCheck state changes
+  useEffect(() => {
+    checkLogin();  // Call checkLogin() function to update loginCheck state
+  }, [loginCheck]);  // Dependency array ensures useEffect runs when loginCheck changes
+
 
   return (
     <div className="nav_container">
@@ -44,3 +51,4 @@ const Navbar = () => {
 }
 
 export default Navbar;
+
